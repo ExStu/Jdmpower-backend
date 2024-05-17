1ae51dfbafe0365ef5863e82b91988cdcdf30c32
Egor Stupenkov <109738127+ExStu@users.noreply.github.com>
2024-05-17 14:42:35 +0300

    port change

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
 	const config = new DocumentBuilder()
-		.setTitle('JdmPower API')
-		.setDescription('Description of all endpoints for JdmPower')
-		.setVersion('1.0')
+		.setTitle("JdmPower API")
+		.setDescription("Description of all endpoints for JdmPower")
+		.setVersion("1.0")
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, document);
+	SwaggerModule.setup("api", app, document);
 
-  app.setGlobalPrefix('api')
-	app.enableCors()
-	await app.listen(4200, "0.0.0.0")
+	app.setGlobalPrefix("api");
+	app.enableCors();
+	await app.listen(3000, "0.0.0.0");
 }
 bootstrap();
