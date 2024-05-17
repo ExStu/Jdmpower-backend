eaa529419ca08bc82eda03bceedf5801e383b145
Egor Stupenkov <109738127+ExStu@users.noreply.github.com>
2024-05-17 14:59:24 +0300

    prod fix

--- dist/src/main.js
+++ dist/src/main.js
@@ -6,15 +6,15 @@ const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = new swagger_1.DocumentBuilder()
-        .setTitle('JdmPower API')
-        .setDescription('Description of all endpoints for JdmPower')
-        .setVersion('1.0')
+        .setTitle("JdmPower API")
+        .setDescription("Description of all endpoints for JdmPower")
+        .setVersion("1.0")
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup('api', app, document);
-    app.setGlobalPrefix('api');
+    swagger_1.SwaggerModule.setup("api", app, document);
+    app.setGlobalPrefix("api");
     app.enableCors();
-    await app.listen(4200, "0.0.0.0");
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
